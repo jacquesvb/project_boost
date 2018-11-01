0f400b60bd59785b769fa065df318c64912b079d
Jacques Van Blokland <jvanblokland@catalyte.io>
2018-10-31 20:41:17 -0700

    Refactor

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+
+	[SerializeField] private Vector3 movementVector = new Vector3(10f, 10f, 10f);
+	[SerializeField] private float period = 2f;
+	
+	// todo remove from inspector later
+	[Range(0,1)] [SerializeField] private float movementFactor; // 0 not moved, 1 for fully moved
+
+	private Vector3 startingPos;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// protect against period is zero
+		if (period <= Mathf.Epsilon)
+		{
+			return;
+		}
+
+		float cycles = Time.time / period; // grows continually from 0
+
+		const float tau = Mathf.PI * 2f;
+		float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+		movementFactor = rawSinWave / 2f + 0.5f;
+		Vector3 offset = movementVector * movementFactor;
+		transform.position = startingPos + offset;
+	}
+}
---
+++ Assets/Rocket.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour
+{
+	[SerializeField] float rcsThrust = 100f;
+	[SerializeField] private float mainThrust = 100f;
+	[SerializeField] float levelLoadDelay = 2f;
+	
+	[SerializeField] private AudioClip mainEngine;
+	[SerializeField] private AudioClip success;
+	[SerializeField] private AudioClip death;
+	
+	[SerializeField] private ParticleSystem mainEngineParticles;
+	[SerializeField] private ParticleSystem successParticles;
+	[SerializeField] private ParticleSystem deathParticles;
+
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+	
+	enum State { Alive, Dying, Transcending }
+
+	private State state = State.Alive;
+
+	private bool collisionsDisabled = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (state == State.Alive)
+		{
+			RespondToThrustInput();
+			RespondToRotateInput();
+		}
+
+		if (Debug.isDebugBuild)
+		{
+			RespondToDebugKeys();
+		}
+	}
+
+	private void RespondToDebugKeys()
+	{
+		if (Input.GetKeyDown(KeyCode.L))
+		{
+			LoadNextLevel();
+		}
+		else if(Input.GetKeyDown(KeyCode.C))
+		{
+			collisionsDisabled = !collisionsDisabled;
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (state != State.Alive || !collisionsDisabled)
+		{
+			return;
+		}
+		
+		switch (collision.gameObject.tag)
+		{
+			case "Friendly":
+				// do nothing
+				break;
+			case "Finish":
+				StartSuccessSequence();
+				break;
+			default:
+				StartDeathSequence();
+				break;
+		}
+	}
+
+	private void StartSuccessSequence()
+	{
+		state = State.Transcending;
+		audioSource.Stop();
+		audioSource.PlayOneShot(success);
+		successParticles.Play();
+		Invoke("LoadNextLevel", levelLoadDelay); // parameterize time
+	}
+	
+	private void StartDeathSequence()
+	{
+		state = State.Dying;
+		audioSource.Stop();
+		audioSource.PlayOneShot(death);
+		deathParticles.Play();
+		Invoke("LoadFirstLevel", levelLoadDelay); // parameterize time
+	}
+
+	private void LoadNextLevel()
+	{
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		int nextSceneIndex = currentSceneIndex++;
+		int numberOfScenes = SceneManager.sceneCountInBuildSettings;
+		if (nextSceneIndex > numberOfScenes)
+		{
+			nextSceneIndex = 0;
+		}
+		SceneManager.LoadScene(nextSceneIndex);
+	}
+	
+	private void LoadFirstLevel()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	private void RespondToThrustInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			ApplyThrust();
+		}
+		else
+		{
+			audioSource.Stop();
+			mainEngineParticles.Stop();
+		}
+	}
+
+	private void ApplyThrust()
+	{
+		rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+		if (!audioSource.isPlaying)
+		{
+			audioSource.PlayOneShot(mainEngine);
+			mainEngineParticles.Play();
+		}
+	}
+
+	private void RespondToRotateInput()
+	{		
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			rigidBody.freezeRotation = true; // take manual control of rotation
+			transform.Rotate(Vector3.forward * rotationThisFrame);
+			rigidBody.freezeRotation = false; // resume physics control of rotation
+		} else if (Input.GetKey(KeyCode.D))
+		{
+			rigidBody.freezeRotation = true; // take manual control of rotation
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
+			rigidBody.freezeRotation = false; // resume physics control of rotation
+		}
+	}
+}
